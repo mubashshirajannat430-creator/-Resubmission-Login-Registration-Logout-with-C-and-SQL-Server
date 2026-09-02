4a26c01035c62d803e883a3d479f4ff61d702e6e
Mubashshira <mubashshirajannat430@gmail.com>
2026-09-02 16:33:48 +0600

    first commit

---
+++ Login and Register/frmLogin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+namespace Login_and_Register
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["db_users"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string login = "SELECT * FROM tbl_users WHERE username = '" + txtUsername.Text +"' and password = '" + txtPassword.Text +"'";
+
+            cmd = new SqlCommand(login, con);
+            SqlDataReader dr = cmd.ExecuteReader();     
+
+            if (dr.Read() == true)
+            {
+                new frmDashboard().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username and Password , Please Try Again" , "Login Faild" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                txtUsername.Focus();
+            }
+        }
+
+        private void checkbxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbxShowPas.Checked)
+            {
+                txtPassword.PasswordChar = '\0';             
+            }
+            else
+            {
+                txtPassword.PasswordChar = '•';
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtUsername.Focus();
+        }
+
+        private void clickRegister_Click(object sender, EventArgs e)
+        {
+            new frmRegister().Show();
+            this.Hide();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye");
+            Application.Exit();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Login and Register/frmRegister.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Login_and_Register
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["db_users"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" && txtPassword.Text == "" && txtConPassword.Text == "")
+            {
+                MessageBox.Show("Username and Password filds are empty", "Register Faild", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (txtPassword.Text == txtConPassword.Text)
+            {
+                con.Open();
+                string register = "INSERT INTO tbl_users VALUES('" + txtUsername.Text + "' , '" + txtPassword.Text + "')";
+                cmd = new SqlCommand(register, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                txtConPassword.Text = "";
+                txtUsername.Focus();
+
+                MessageBox.Show("Your Account has been Sucessfully Created" , "Registration Sucess" , MessageBoxButtons.OK , MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Password does not matched , Please Re-enter" , "Register Failed" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+                txtPassword.Text = "";
+                txtConPassword.Text = "";
+                txtPassword.Focus();
+            }
+        }
+
+        private void checkbxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbxShowPas.Checked)
+            {
+                txtPassword.PasswordChar = '\0';
+                txtConPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                txtPassword.PasswordChar = '•';
+                txtConPassword.PasswordChar = '•';
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtConPassword.Text = "";
+            txtUsername.Focus();
+        }
+
+        private void clickLogin_Click(object sender, EventArgs e)
+        {
+            new frmLogin().Show();
+            this.Hide();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye");
+            Application.Exit();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmRegister_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
